e22dd976180a753c4a75ef36397cb8a6dd1319d7
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-09 16:23:18 +0200

    Update squareToCircle.cs

--- input/squareToCircle.cs
+++ input/squareToCircle.cs
@@ -1,11 +1,16 @@
+private Vector2 mapSquareToCircle(Vector2 input)
+{
+	// map square input to circle, to maintain uniform speed in all directions
+	return new Vector2(
+		input.x * Mathf.Sqrt(1 - input.y * input.y * 0.5f),
+		input.y * Mathf.Sqrt(1 - input.x * input.x * 0.5f)
+	);
+}
+
 // get input
 var input = new Vector2(
 	Input.GetAxisRaw("Horizontal"),
 	Input.GetAxisRaw("Vertical")
 );
 
-// map square input to circle, to maintain uniform speed in all directions
-var inputCircle = new Vector2(
-	input.x * Mathf.Sqrt(1 - input.y * input.y * 0.5f),
-	input.y * Mathf.Sqrt(1 - input.x * input.x * 0.5f)
-);
+var inputMapped = mapSquareToCircle(input);
