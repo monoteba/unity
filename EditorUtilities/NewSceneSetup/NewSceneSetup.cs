6ec7063ba3a7ebe6f199ae7edbb1ce64d3ad814e
Morten Andersen <11890765+mortenblaa@users.noreply.github.com>
2020-05-27 15:33:56 +0200

    Add files via upload

---
+++ EditorUtilities/NewSceneSetup/NewSceneSetup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class NewSceneSetup : Editor
+{
+    static NewSceneSetup()
+    {
+        EditorSceneManager.newSceneCreated += OnNewScene;
+    }
+
+    private static void OnNewScene(Scene scene, UnityEditor.SceneManagement.NewSceneSetup setup, NewSceneMode mode)
+    {
+        Camera.main.backgroundColor = Color.black;
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+        Camera.main.transform.position = Vector3.zero;
+
+        var light = FindObjectOfType<Light>();
+        light.color = Color.white;
+        light.transform.position = Vector3.one;
+
+        RenderSettings.skybox = null;
+        RenderSettings.ambientIntensity = 0f;
+        RenderSettings.ambientLight = Color.black;
+        RenderSettings.ambientMode = AmbientMode.Flat;
+        Lightmapping.bakedGI = false;
+    }
+}
